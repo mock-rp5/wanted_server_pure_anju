bef70d271c5af4abd0e9a5e03b827c554f11669d
Yunseok Kang <mok02198@gmail.com>
2022-05-25 21:44:10 +0900

    feat: 채용공고 북마크 취소 API 및 북마크 조회 API 구현 (#14)

--- src/main/java/com/example/demo/config/BaseResponseStatus.java
+++ src/main/java/com/example/demo/config/BaseResponseStatus.java
@@ -49,6 +49,11 @@ public enum BaseResponseStatus {
     POST_BOOKMARK_FAIL(false, 3050, "중복된 북마크 입니다."),
     UPDATE_FAIL_BOOKMARK(false, 3051, "북마크 생성에 실패하였습니다."),
 
+    FAIL_CANCEL_BOOKMARK(false, 3002, "북마크 해제에 실패하였습니다."),
+
+
+
+
     /**
      * 4000 : Database, Server 오류
      */
