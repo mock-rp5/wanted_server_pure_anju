641b3358548bad6e43ecafd3a93238b36f9579f0
Yunseok Kang <mok02198@gmail.com>
2022-05-30 16:04:05 +0900

    feat: 회사 팔로우 생성 API 구현, 회사 팔로우 취소 API 구현 (#32)
    * feat: 메인화면 이벤트 조회 API 구현
    
    * feat: 메인화면 아티클 조회 API 구현
    
    * feat: 회사 팔로우 생성 API 구현
    
    * feat: 회사 팔로우 취소 API 구현

---
+++ src/main/java/com/example/demo/src/follow/FollowController.java
@@ -0,0 +1,69 @@
+package com.example.demo.src.follow;
+
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
+import com.example.demo.utils.JwtService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/app/follows")
+public class FollowController {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private final FollowProvider followProvider;
+    @Autowired
+    private final FollowService followService;
+    @Autowired
+    private final JwtService jwtService;
+
+    public FollowController(FollowProvider followProvider, FollowService followService, JwtService jwtService) {
+        this.followProvider = followProvider;
+        this.followService = followService;
+        this.jwtService = jwtService;
+    }
+
+    /**
+     * 회사 팔로우 생성 API
+     * [POST]
+     */
+    @ResponseBody
+    @PostMapping("/companies/{companyIdx}")
+    public BaseResponse<String> createFollow(@PathVariable("companyIdx") int companyIdx) {
+
+
+        try {
+            Long userIdx = jwtService.getUserIdx();
+
+            followService.createFollow(companyIdx, userIdx);
+            String result = "북마크에 추가하였습니다.";
+            return new BaseResponse<>(result);
+
+        } catch (BaseException exception) {
+            return new BaseResponse<>((exception.getStatus()));
+        }
+
+    }
+
+    /**
+     * 채용공고 좋아요 취소 API
+     * [PATCH]
+     */
+    @ResponseBody
+    @PatchMapping("companies/{companyIdx}")
+    public BaseResponse<String> cancelFollow(@PathVariable("companyIdx") int companyIdx){
+        try{
+            Long userIdx = jwtService.getUserIdx();
+
+            followService.cancelFollow(companyIdx, userIdx);
+
+            String result = "";
+            return new BaseResponse<>(result);
+        } catch(BaseException exception){
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+}
